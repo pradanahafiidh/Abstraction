d681c4569a65d3e95eac93e13571cbc0013dc316
pradanahafiidh <pradanahafidh@gmail.com>
2020-06-11 14:16:47 +0700

    Tugas Lab 7 Abstraction

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Abtraction.abtractionClass;
+using Abtraction.Interface;
+
+namespace Abtraction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine();
+            Console.WriteLine("Konsep Abstraksi Menggunakan Abstract Class. ");
+            Console.WriteLine();
+            hewan Hewan;
+
+            Hewan = new herbivora();
+            Hewan.jenisMakanan();
+
+            Hewan = new karnivora();
+            Hewan.jenisMakanan();
+
+            Hewan = new omnivora();
+            Hewan.jenisMakanan();
+
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("Konsep Abstraksi Menggunakan Interface. ");
+            Console.WriteLine();
+            IReproduksi reproduksiHewan;
+
+            reproduksiHewan = new vivipar();
+            reproduksiHewan.berkembangBiak();
+
+            reproduksiHewan = new ovipar();
+            reproduksiHewan.berkembangBiak();
+
+            reproduksiHewan = new ovovivipar();
+            reproduksiHewan.berkembangBiak();
+
+            Console.ReadKey();
+        }
+    }
+}
